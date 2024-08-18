2ff8a1816f4db0939308c2432046d8cd1c2e960d
RoxanEGarcia <leyra627@gmail.com>
2024-08-17 21:46:10 -0300

    V1.2

--- src/index.js
+++ src/index.js
@@ -13,5 +13,6 @@ app.use(express.json());
 app.use(router);
 
 app.listen(app.get('port'), () => {
-        console.log(`Server on port ${app.get('port')}`);
+    
+    console.log(`Server on port ${app.get('port')}`);
 })
--- src/libros.js
+++ src/libros.js
@@ -1,41 +1,90 @@
 import {pool} from './database.js';
 
 class LibroController{
-
+  
   async  getAll(req, res){
-        const [result] = await pool.query('Select * FROM libros');
+    try{
+        const [result] = await pool.query('Select * FROM libros' );
         res.json(result);
+
+      }catch(error){
+        res.status(500).json({error: 'SR: Usuario se produjo un error al Listar libros', detalle: error});
+    }
     }
 
-
-async add(req, res){
-    const libro = req.body;
-    const [result] = await pool.query('INSERT INTO libros(nombre,autor,categoria,anoPublicacion,isbn) VALUES (?,?,?,?,?)', 
-        [libro.nombre, 
-        libro.autor,
-        libro.categoria,
-        libro.anoPublicacion,
-        libro.isbn]); 
-    res.json({"Id insertado": result.insertId});
-}
-
-async delete(req, res){
-  const libro = req.body;
-  const [result] = await pool.query(`DELETE FROM libros WHERE id=(?)`, [libro.id]);
-  res.json({"Registros eliminados":result.affectedRows});
-}
-
-async update (req, res){
-    const libro = req.body;
-    const [result] = await pool.query(`UPDATE  libros SET nombre=(?), autor=(?), anoPublicacion=(?), isbn=(?) WHERE id=(?)`,
-         [libro.nombre, 
-            libro.autor,
-            libro.categoria,
-            libro.anoPublicacion,
-            libro.isbn]);
-    res.json({"Registros Actualizados":result.changedRows});
+    async getOne (req, res){
+      const libro = req.body;
+      const {id} = req.params;
+      try {
+         const [result] = await pool.query(`Select * FROM libros WHERE id= ?`, [id]);
+         if(result.lenght === 0){
+          res.status(404).json({message:"Libro encontrado"});
+          }else{
+            res.json(result[0]);
+          }
+          
+        }catch (error) {
+            res.status(500).json({message: 'Error al obtener el libro', error});
+  
+      }
   }
 
+async add(req, res){
+  const libro = req.body;
+  const { nombre, autor, categoria, año_publicacion, ISBN} = req.body;
+    if (!nombre || !autor || !categoria || !año_publicacion || !ISBN ){
+      return res.status(400).json({error: 'Todos los campos son obligatorios'});
+     }
+try {
+  const [result] = await pool.query('INSERT INTO libros(nombre,autor,categoria,año_publicacion,ISBN) VALUES (?,?,?,?,?)', 
+      [libro.nombre, 
+      libro.autor,
+      libro.categoria,
+      libro.año_publicacion,
+      libro.ISBN]); 
+  res.json({"Sr:Usuario Libro inscorporado con éxito": result.insertId,
+ 
+    });
+
+} catch (error) {
+  res.status(500).json({ message: "Sr:Usuario se produjo un error al agregar el libro" });
+}
+ }
+
+
+async delete(req, res){
+ try {
+  const libro = req.body;
+  const [result] = await pool.query(`DELETE FROM libros WHERE ISBN=?`, [libro.ISBN]);
+  if(result.affectedRows > 0){
+     res.json({"Registros eliminados":result.affectedRows});
+     }else{
+     res.status(404).json({ mensaje: 'SR:Usuario no se encuentra su libro en la base de datos para ser eliminado.'});
+    }
+
+  } catch (error) {
+    console.error('Error al eliminar el libro:', error.message);
+    res.status(500).json({ message: 'Error al eliminar el libro', error: error.message });
+  }
 }
 
+
+async update (req, res){
+ 
+    try {
+      const libro = req.body;
+      const [result] =await pool.query(`UPDATE libros SET nombre=?, autor=?, categoria=?, año_publicacion=?, ISBN=? WHERE id=?`,
+        [libro.nombre,
+        libro.autor,
+        libro.categoria,
+        libro.año_publicacion,
+        libro.ISBN,
+        libro.id]);
+      res.json({"Sr: Usuario  se actualizo con exito su registro": result.changedRows}); 
+    } catch (error) {
+      console.error('Sr: Usuario se produjo un error al actualizar el libro:', error);
+      res.status(500).json({ mensaje: 'Sr: Usuario ocurrió un error al intentar actualizar el libro.' });
+    }
+  }
+}
 export const libro = new LibroController();
